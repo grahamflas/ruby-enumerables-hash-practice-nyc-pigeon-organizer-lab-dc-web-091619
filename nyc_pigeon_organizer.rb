a37a7a3ff36f2e5cab7fa24fde1b25a68262d3b5
Graham Flaspoehler <grahamflas@gmail.com>
2019-09-10 20:55:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
     new_hash = Hash.new
+
+    data.each do |key, value|
+        value.each do |new_value, name_key|
+            name_key.each do |name|
+                if !new_hash[name]
+                    new_hash[name] = {}
+                end
+            end
+        end
+    end
+    new_hash
 end
