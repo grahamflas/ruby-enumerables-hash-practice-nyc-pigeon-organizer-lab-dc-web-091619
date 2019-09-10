4571057cb8273e93dc27c624bbce9b6c226b688c
Graham Flaspoehler <grahamflas@gmail.com>
2019-09-10 22:12:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,11 +2,21 @@ def nyc_pigeon_organizer(data)
     new_hash = Hash.new
 
     data.each do |key, value|
+        #key = color, gender, lives
+        #value = hashes with above keys
         value.each do |new_value, name_key|
+            # new_value = `colors`, `genders`, `domiciles`
+            # name_key = `array of names`
             name_key.each do |name|
                 if !new_hash[name]
                     new_hash[name] = {}
                 end
+
+                if !new_hash[name][key]
+                    new_hash[name][key] = []
+                end
+
+                new_hash[name][key] << new_value.to_s
             end
         end
     end
